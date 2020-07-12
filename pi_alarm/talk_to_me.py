86a4a51175c2a66ea7bd5ebed222dbb37f59f5e5
Martijn <mvanloocke@gmail.com>
2020-07-12 18:57:00 +0200

    Fix TalkToMe for linux

--- pi_alarm/talk_to_me.py
+++ pi_alarm/talk_to_me.py
@@ -10,5 +10,7 @@ class TalkToMe:
             subprocess.run(['tools/vlc-3.0.11/vlc', '-I dummy', '--dummy-quiet',
                             'http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q='+urllib.parse.quote(text)+'&tl=en', 'vlc://quit'])
         else:
-            subprocess.run(['/usr/bin/mplayer', '-ao alsa', '-really-quiet', '-noconsolecontrols',
+            subprocess.run(['cvlc', '--play-and-exit', '-I dummy', '--quiet',
                             'http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q='+urllib.parse.quote(text)+'&tl=en'])
+            # subprocess.run(['/usr/bin/mplayer', '-ao alsa', '-really-quiet', '-noconsolecontrols',
+                            # 'http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q='+urllib.parse.quote(text)+'&tl=en'])
